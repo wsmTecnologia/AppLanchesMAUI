9ba396d85542ec60b090fa0edc260a24a47264b4
WANDERLEY DE SOUZA MACHADO <2008.wsm@gmail.com>
2024-12-21 09:36:28 -0300

    Criando projetos backend e app MAUI

---
+++ src/WSM.ApiECommerce/Controllers/CategoriasController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSM.ApiECommerce.Repositories;
+
+namespace WSM.ApiECommerce.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly ICategoriaRepository categoriaRepository;
+
+    public CategoriasController(ICategoriaRepository categoriaRepository)
+    {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var categorias = await categoriaRepository.GetCategorias();
+        return Ok(categorias);
+    }
+}
---
+++ src/WSM.ApiECommerce/Repositories/CategoriaRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WSM.ApiECommerce.Context;
+using WSM.ApiECommerce.Entities;
+
+namespace WSM.ApiECommerce.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly AppDbContext dbContext;
+
+    public CategoriaRepository(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Categoria>> GetCategorias()
+    {
+        return await dbContext.Categorias.ToListAsync();
+    }
+}
